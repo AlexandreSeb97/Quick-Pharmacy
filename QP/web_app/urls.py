7aedacc8106ee43643c4d9ed2beb845f0f9ed62a
nehri97 <doublerico7@gmail.com>
2015-03-21 18:08:41 -0400

    urls modification

--- QP/web_app/urls.py
+++ QP/web_app/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('web_app.views',
-	url(r'^accueil$', 'home'),
-	url(r'^resultats$', 'resultats'),
+urlpatterns = patterns('',
+	url(r'^$', 'web_app.views.home', name='home'),
 )
