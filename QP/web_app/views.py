1f5e07f9f8e68b56a84e753c201bb7f74a8c7443
nehri97 <doublerico7@gmail.com>
2015-03-21 20:00:21 -0400

    commit 3

--- QP/web_app/views.py
+++ QP/web_app/views.py
@@ -1,33 +1,19 @@
-from django.http import HttpResponse
+from django.http import HttpRequest
 from django.shortcuts import render
+from django.template import RequestContext
+
 
 # Create your views here.
 def home(request):
-	text="""<!DOCTYPE html>
-		 <html>
-		 <head>
-			<meta charset="utf-8"/>
-			<title>Bienvenue sur PharmaOnline</title>
-			<link rel="stylesheet" type="text/css" href="base.css" />
-		 </head>
-		 <body>
-			<h1>QuickPharma</h1>
-			<form method="GET" action={% url "page2.html" %} accept-charset="UTF-8">
-			<input type="text" placeholder="Besoin d'un medicament?" name="search_entry" />
-			<br>
-			<input type="submit" value="recherche" />
-			</form>
-
-		 <p id="para1">Bonjour et bienvenue sur pharmaOnline !
-		 Ce site a été créé pour aider les gens dans le besoin à trouver les medicaments et les pharmacies
-		 qui vendent ces medicaments. Cependant pour certains de ces medicaments il vous faudra une recommendation
-		 d'un medecin.
-		 </p> 
-
-		 </body>
-
-		 </html>"""
-	return HttpResponse(text)
+	asseert isinstance(request, HttpRequest)
+	return render(
+		request,
+		'wep_app/index.html',
+		context_instance = RequestContext(request,
+		{
+			title: 'Home',
+		})
+	)
 def resultats(request):
 	text="""<!DOCTYPE html>
 		 <html>
