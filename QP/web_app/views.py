dad380463be66bbf72ced3dc5d4a8aca70aba162
nehri97 <doublerico7@gmail.com>
2015-03-23 10:27:44 -0400

    this is agony

--- QP/web_app/views.py
+++ QP/web_app/views.py
@@ -5,26 +5,13 @@ from django.template import RequestContext
 
 # Create your views here.
 def home(request):
-	asseert isinstance(request, HttpRequest)
+	assert isinstance(request, HttpRequest)
 	return render(
 		request,
 		'wep_app/index.html',
 		context_instance = RequestContext(request,
 		{
-			title: 'Home',
+			'title': 'Home',
+			'yeR': datetime.now().year,
 		})
 	)
-def resultats(request):
-	text="""<!DOCTYPE html>
-		 <html>
-		 <head>
-			<meta charset="utf-8"/>
-			<title> Resultats de la recherche</title>
-			<link rel="stylesheet" type="text/css" href="base.css" />
-		 </head>
-		 <body>
-			<h1 id="one"> QuickPharma </h1>
-			<p><Dans cette page vous retrouverez les resultats de votre recherche</p>
-		</body>
-		</html>"""
-	return HttpResponse(text)
