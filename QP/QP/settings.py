c3e22d8de8649d3c09f81d7a68a0fd69a596bc05
nehri97 <doublerico7@gmail.com>
2015-03-30 01:07:33 -0400

    30/03/15 - 3

--- QP/QP/settings.py
+++ QP/QP/settings.py
@@ -11,6 +11,15 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -64,6 +73,10 @@ DATABASES = {
     }
 }
 
+import dj-database-url
+DATABASES['default'] = dj-database-url.config()
+
+DATABASES['default']['ENGINE'] = 'django_postgrespool'
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
@@ -80,10 +93,3 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-	os.path.join(BASE_DIR, 'static'),
-)
-STATIC_ROOT = 'staticfiles'
