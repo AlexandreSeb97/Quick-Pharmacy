1692ea9baed35b9d611ebb0266bd0c8069cf75e8
Alexandre Erich Sebastien Georges <erichaitian97@gmail.com>
2015-03-30 10:52:03 -0400

    more

--- QP/QP/urls.py
+++ QP/QP/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from web_app import views
 
 urlpatterns = patterns('',
-    url(r'^web_app/', include('web_app.urls')),
+	url(r'^$', 'web_app.views.home', name='home'),
+	url(r'^web_app/', include('web_app.urls')),
 )
 
